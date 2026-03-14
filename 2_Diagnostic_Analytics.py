e76824ab01c6cf161004093b54eadc3a914f8663
erzorigmaral2 <erzorigmaral2@gmail.com>
2026-03-14 21:53:18 +0800

    Update 2_Diagnostic_Analytics.py

--- 2_Diagnostic_Analytics.py
+++ 2_Diagnostic_Analytics.py
@@ -1,29 +1,27 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 
-df = pd.read_csv("data/mongolia_legal_survey_synthetic_2000.csv")
+df = pd.read_csv("data/mongolia_legal_survey_synthetic_dataset_2000.csv")
 
 st.title("Diagnostic Analytics")
 
-st.subheader("Main Challenges When Finding Lawyers")
+st.subheader("Main Difficulty Finding Lawyers")
 
-fig = plt.figure()
+fig = px.bar(
+    df["MainDifficulty"].value_counts().reset_index(),
+    x="index",
+    y="MainDifficulty"
+)
 
-df["MainDifficulty"].value_counts().plot(kind="bar")
+st.plotly_chart(fig)
 
-st.pyplot(fig)
+st.subheader("Difficulty vs Legal Issue")
 
+fig2 = px.box(
+    df,
+    x="LegalIssue",
+    y="TimeToFindLawyer"
+)
 
-st.subheader("Satisfaction vs Pay Likelihood")
-
-grouped = df.groupby("Satisfaction")["PayLikelihood"].mean()
-
-fig = plt.figure()
-
-grouped.plot(kind="bar")
-
-plt.xlabel("Satisfaction Score")
-plt.ylabel("Average Pay Likelihood")
-
-st.pyplot(fig)
+st.plotly_chart(fig2)
