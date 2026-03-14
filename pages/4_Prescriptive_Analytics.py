e9915a1d64cd43a92811eeb294f3ef932a2fe403
erzorigmaral2 <erzorigmaral2@gmail.com>
2026-03-14 22:52:43 +0800

    Update 4_Prescriptive_Analytics.py

--- pages/4_Prescriptive_Analytics.py
+++ pages/4_Prescriptive_Analytics.py
@@ -3,11 +3,9 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 import plotly.express as px
 
-df = pd.read_csv("data/mongolia_legal_survey_synthetic_dataset_2000.csv")
-
 st.title("Prescriptive Analytics")
 
-st.subheader("Association Rule Mining")
+df = pd.read_csv("data/mongolia_legal_survey_synthetic_dataset_2000.csv")
 
 data = pd.get_dummies(df[
     ["MainDifficulty","PreferredConsultation","TrustFactor"]
@@ -17,7 +15,7 @@ freq = apriori(data, min_support=0.05, use_colnames=True)
 
 rules = association_rules(freq, metric="confidence", min_threshold=0.5)
 
-st.write("Top Rules")
+st.subheader("Top Association Rules")
 
 st.dataframe(rules.head())
 
