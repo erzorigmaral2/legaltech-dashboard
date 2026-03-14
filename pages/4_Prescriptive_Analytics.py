9424666265b858d81e0d1df34ece937626d18227
erzorigmaral2 <erzorigmaral2@gmail.com>
2026-03-14 22:57:17 +0800

    Update 4_Prescriptive_Analytics.py

--- pages/4_Prescriptive_Analytics.py
+++ pages/4_Prescriptive_Analytics.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
-import plotly.express as px
 
 st.title("Prescriptive Analytics")
 
 df = pd.read_csv("data/mongolia_legal_survey_synthetic_dataset_2000.csv")
 
-data = pd.get_dummies(df[
-    ["MainDifficulty","PreferredConsultation","TrustFactor"]
-])
+data = pd.get_dummies(df[[
+    "MainDifficulty",
+    "PreferredConsultation"
+]])
 
 freq = apriori(data, min_support=0.05, use_colnames=True)
 
@@ -18,12 +18,3 @@ rules = association_rules(freq, metric="confidence", min_threshold=0.5)
 st.subheader("Top Association Rules")
 
 st.dataframe(rules.head())
-
-fig = px.scatter(
-    rules,
-    x="confidence",
-    y="lift",
-    hover_data=["antecedents","consequents"]
-)
-
-st.plotly_chart(fig)
