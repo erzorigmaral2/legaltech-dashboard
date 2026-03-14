40241edfb654ddeb6392d5c24200b3c6bb0ea5e2
erzorigmaral2 <erzorigmaral2@gmail.com>
2026-03-14 10:41:21 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,1 @@
-import pandas as pd
-
-def load_data():
-    df = pd.read_csv("data/mongolia_legal_survey_synthetic_2000.csv")
-    return df
+df = pd.read_csv("mongolia_legal_survey_synthetic_2000.csv")
